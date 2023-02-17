00b1397747ab3703501d58a4635747bcd4e7c8bb
Alice Bagoud <99303912+alicebagoud@users.noreply.github.com>
2023-02-16 20:31:42 -0500

    main

--- game.js
+++ game.js
@@ -27,20 +27,19 @@ function setCanvasSize() {
 }
 
 function startGame() {
-
     console.log({canvasSize, elementsSize});
 
     game.font = elementsSize + 'px Verdana';
     game.textAlign = 'end';
 
-    const map = maps[1];
-    const mapRows = maps.trim().split('\n');
+    const map = maps[0];
+    const mapRows = map.trim().split('\n');
     const mapRowCols = mapRows.map(row => row.trim().split(''));
     console.log({map, mapRows, mapRowCols});
 
     for (let row = 1; row <= 10; row++) {
         for (let col = 1; col <= 10; col++) {
-            game.fillText(emojis[mapRowCols[row - 1][col - 1]], elementsSize * row, elementsSize * col);
+            game.fillText(emojis[mapRowCols[row - 1][col - 1]], elementsSize * col, elementsSize * row);
         }
     }
 }
